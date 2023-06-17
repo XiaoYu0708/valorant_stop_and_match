187e576ea3a4cadec51b091bc56a7dca28d81f8d
XiaoYu <weitsungyu@gmail.com>
2023-06-17 11:16:40 +0800

    Update v1.0.5

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:valorant_stop_and_match/main.dart';
+import 'package:Valorant_Match/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
